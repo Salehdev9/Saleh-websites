20199baa02ce7f03ba91cb1a09d29932d72fed21
Salehdev9 <salehdeveloper9@gmail.com>
2024-08-19 10:39:45 +0300

    Add files via upload

---
+++ dark-mode2.js
@@ -0,0 +1,8 @@
+let img1 = document.getElementById("home-bg-img1");
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  console.log("Chal raha houn");
+
+  img1.style.visibility = "hidden";
+  
+}
