255af8a2d5320bd3d8643954677c8304158a346d
Salehdev9 <salehdeveloper9@gmail.com>
2024-08-27 20:53:33 +0300

    Update dark-mode2.js

--- dark-mode2.js
+++ dark-mode2.js
@@ -1,4 +1,1 @@
-let email011 = document.getElementById("contact-number");
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  email011.style.color = "red";
-}
+
