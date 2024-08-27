372e89807a3fe5db5a9342d961472e22e174c7e4
Salehdev9 <salehdeveloper9@gmail.com>
2024-08-27 20:25:04 +0300

    Update dark-mode2.js

--- dark-mode2.js
+++ dark-mode2.js
@@ -1,17 +1,9 @@
 
 let img1 = document.getElementById("home-bg-img1");
-let img2 = document.getElementById("home-bg-img2");
-let img3 = document.getElementById("services-left-img");
-let img4 = document.getElementById("portfolio-top-left");
-let img5 = document.getElementById("portfolio-bottom-right");
+
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   console.log("Chal raha houn");
 
-  img1.style.border = "2px solid red";
-  img1.style.visibility = "hidden";
-  img2.style.visibility = "hidden";
-  img3.style.visibility = "hidden";
-  img4.style.visibility = "hidden";
-  img5.style.visibility = "hidden";
+  img1.style.width = "100px";
 }
