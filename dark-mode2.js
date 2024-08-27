fb2c25a0ad87818a1144b83e96bfdc8007cb110b
Salehdev9 <salehdeveloper9@gmail.com>
2024-08-27 20:35:42 +0300

    Update dark-mode2.js

--- dark-mode2.js
+++ dark-mode2.js
@@ -1,9 +1,3 @@
-
-let img1 = document.getElementById("contact-number");
-
-
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  console.log("Chal raha houn");
-
-  img1.style.innerHTML = "321";
+  alert("I m working");
 }
